e021c5d1a4fb6bc74d5cdce4969f120d731c2685
lhoang123 <164918769+lhoang123@users.noreply.github.com>
2024-04-22 16:42:05 +0000

    created and pushed code

---
+++ app/controllers/zebra_controller.rb
@@ -0,0 +1,20 @@
+class ZebraController < ApplicationController
+  def puppy
+    render({:template => "game_templates/homepage"})
+  end
+  
+  def kitten
+    @num_dice = params.fetch("number_of_dice").to_i
+    @sides = params.fetch("how_many_sides").to_i
+    @rolls = []
+
+    @num_dice.times do
+      die = rand(1..@sides)
+
+      @rolls.push(die)
+    end 
+  render({:template => "game_templates/flexible"})
+  
+  end 
+
+end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get("/", {:controller => "zebra", :action => "puppy"})
 
+  get("/dice/:number_of_dice/:how_many_sides", {:controller => "zebra", :action => "kitten"})
 end
